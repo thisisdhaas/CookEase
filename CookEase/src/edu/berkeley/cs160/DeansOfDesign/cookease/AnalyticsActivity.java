a20f9e2eb17f8c9b4ab1cf9b9ae33877b42ff775
Danny N. Chang <dannynchang@gmail.com>
2014-04-19 20:36:46 -0700

    Implemented parts of Analytics

--- CookEase/src/edu/berkeley/cs160/DeansOfDesign/cookease/AnalyticsActivity.java
+++ CookEase/src/edu/berkeley/cs160/DeansOfDesign/cookease/AnalyticsActivity.java
@@ -1,8 +1,21 @@
 package edu.berkeley.cs160.DeansOfDesign.cookease;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
@@ -11,6 +24,8 @@ public class AnalyticsActivity extends Activity {
 
 	private static TextView homeText;
 	private static TextView notificatonsText;
+	private long waterTime = 0;
+	private long microwaveTime = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,4 +77,92 @@ public class AnalyticsActivity extends Activity {
 		return true;
 	}
 
+	public void startTime(String type) {
+		// Record start time
+		if (type == "water") {
+			waterTime = System.currentTimeMillis();
+		} else if (type == "microwave") {
+			microwaveTime = System.currentTimeMillis();
+		}
+	}
+	
+	public void finishTime(String type) {
+		// Record finish time, then subtract from start time to get total duration
+		long time = System.currentTimeMillis();
+		String fileName = null;
+		if (type == "water") {
+			time -= waterTime;
+			fileName = "waterAnalyticsRecord";
+		} else if (type == "microwave") {
+			time -= microwaveTime;
+			fileName = "microwaveAnalyticsRecord";
+		}
+		// Record this in either microwave or water file 
+		// E.g. 2014-01: 100.42 213.76
+		Calendar cal = Calendar.getInstance();  
+	    int year = cal.get(cal.YEAR);  
+	    int month = cal.get(cal.MONTH)+1; //zero-based
+	    String curDate;
+	    if (month < 10) {
+		    curDate = year+"-"+"0"+month;
+	    } else {
+		    curDate = year+"-"+month;
+	    }
+	    
+	  //reading file line by line in Java using BufferedReader       
+        FileInputStream fis = null;
+        BufferedReader reader = null;
+        Boolean dateFound = false;
+      
+        try {
+        	// Open file and inputstream and stuff like that 
+        	File myFile = new File(fileName);
+        	if (myFile.exists()) {
+        		fis = new FileInputStream(fileName);
+        	} else {
+            // File not found?
+            	FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+            	fos.close();
+                fis = new FileInputStream(fileName);
+            }
+            reader = new BufferedReader(new InputStreamReader(fis));
+          
+            System.out.println("Reading File line by line using BufferedReader");
+            
+            // Look for our "year-month" in first 7 bytes, maybe just skip to last line and check?
+            String line = reader.readLine();
+            String date = line.substring(0, 7);
+            while(line != null){
+            	if (date == curDate) {
+            		dateFound = true;
+            	}
+                line = reader.readLine();
+            }                     
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(AnalyticsActivity.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(AnalyticsActivity.class.getName()).log(Level.SEVERE, null, ex);
+          
+        } finally {
+            try {
+                FileWriter fw = new FileWriter(fileName, true); //the true will append the new data
+            	if (dateFound) {
+            		// Append time to end of string
+            		fw.write(" "+time); //appends the string to the file   
+            	} else {
+            		// Create new line in the file with time
+            		fw.write("\n"+curDate+" "+time); //appends the string to the file   
+            	}
+            	fw.close();
+                reader.close();
+                fis.close();
+            } catch (IOException ex) {
+                Logger.getLogger(AnalyticsActivity.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        }		
+		
+/*		FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
+		fos.write(string.getBytes());
+		fos.close();*/
+	}
 }
